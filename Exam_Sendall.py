89b16d281268116cf1d4a78880d1f616ae70f28a
Ponthawan <s6506022610031@email.kmutnb.ac.th>
2024-07-09 11:55:14 +0700

    save

---
+++ Exam_Sendall.py
@@ -0,0 +1,55 @@
+# Echo server progeam
+import socket
+
+HOST = ''
+PORT = 50007
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
+
+#Echo cilent program
+import socket
+
+HOST = 'daring.cwi.nl'
+PORT = 50007
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+print('Received', repr(data))
+
+#Echo server program
+import socket
+import sys
+
+HOST = None
+PORT = 50007
+s = None
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
+                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+    af, socktype, proto, canonname, sa = res
+    try:
+        s = socket.socket(af, socktype, proto)
+    except OSError as msg:
+        s.close()
+        s = None
+        continue
+    break
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+conn, addr = s.accept()
+with conn:
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        conn.send(data)
+        
